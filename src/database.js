4696cf046651a515329cf2c28d66115f0eed0b28
Jorge <jhessle04@gmail.com>
2020-01-15 19:18:45 -0300

    po

--- src/database.js
+++ src/database.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose');
 
   mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false 
   }) 
       .then(db => console.log(`DB is connected`))
       .catch(err => console.error(err));  
